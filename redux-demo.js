28829bc8dc4bd46660af7639f53db3f08464da68
Jaehyun Bhang <ryanb@Jaehyunui-MacBookPro.local>
2024-04-28 11:37:26 +0900

    feat: dispatching with custom actions

--- redux-demo.js
+++ redux-demo.js
@@ -1,23 +1,36 @@
-const redux = require('redux'); // import Redux from the Redux package with this syntax
+// import Redux from the Redux package with this syntax
+const redux = require('redux');
 
-// Reducer function
+// Reducer function - default state written in parameter.
 const counterReducer = (curState = {counter: 0}, action) => {
-    return {
-        counter: curState.counter + 1
-    };
+    if (action.type === 'increment') {
+        return {
+            counter: curState.counter + 1
+        };
+    }
+    if (action.type === 'decrement') {
+        return {
+            counter: curState.counter - 1
+        };
+    }
+    return curState;
 };
 
 // Store Generation
+// Call the reducer function once when the store is initialized.
 const store = redux.createStore(counterReducer);
 
-// Subscriber function Triggered when the data is changed!
+// Subscriber Function
 const counterSubscriber = () => {
     const latestState = store.getState(); // get the latest store after update
     console.log(latestState);
 };
 
+// Triggered when the data is changed!
 store.subscribe(counterSubscriber);
 
-// dispatching an action
+// Dispatching an action
 store.dispatch({type: 'increment'}); // type must be a different string.
+store.dispatch({type: 'decrement'});
+store.dispatch({type: 'increment'});
 
