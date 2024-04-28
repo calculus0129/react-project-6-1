2df356d853341305b475c54667b17285c3a4306d
calculus0129 <ryanbhang129@naver.com>
2024-04-28 15:16:22 +0900

    feat: manage toggleState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,8 +7,11 @@ import { useSelector, useDispatch } from 'react-redux';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state=>state.counter);
+  const show = useSelector(state => state.showCounter);
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'});
+  };
 
   const incrementHandler = (amt) => {
     dispatch({type: 'increment', amount: amt});
@@ -20,7 +23,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={() => incrementHandler()}>increment</button>
         <button onClick={() => incrementHandler(10)}>increment by 10</button>
