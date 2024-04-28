6265c4e3963335cda06c747379bceb257a6383d9
calculus0129 <ryanbhang129@naver.com>
2024-04-28 17:17:48 +0900

    feat: manage multiple slices

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {useSelector(state=>state.auth.isAuthenticated)?<UserProfile />:<Auth />}
       <Counter />
     </Fragment>
   );
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,6 +1,9 @@
 import classes from './Auth.module.css';
+import { actions } from '../store';
+import { useDispatch } from 'react-redux';
 
 const Auth = () => {
+  const dispatch = useDispatch();
   return (
     <main className={classes.auth}>
       <section>
@@ -13,7 +16,7 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button>Login</button>
+          <button onClick={() => dispatch(actions.authAct.login())}>Login</button>
         </form>
       </section>
     </main>
