567afa62d9bb6c13a3f08970d647a77c8bc67472
calculus0129 <ryanbhang129@naver.com>
2025-01-28 13:55:16 +0900

    refactor: provide store to the highest-level component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import ReduxProvider from "./providers";
 // import { Geist, Geist_Mono } from "next/font/google";
 // import "./globals.css";
 
@@ -62,7 +63,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {/* <body className={`${geistSans.variable} ${geistMono.variable}`}> */}
-        {children}
+        <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
   );
